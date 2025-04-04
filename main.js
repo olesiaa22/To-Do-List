b120f7a38bb5122647aa162365bc2523d5c3bc2c
olesiaa22 <olesiashpotak22@gmail.com>
2025-04-04 11:19:44 +0300

    add complete task

--- main.js
+++ main.js
@@ -1,18 +1,24 @@
 const inputTask = document.getElementById("input");
 const addBtn = document.getElementById("add");
 const taskContainer = document.getElementById("tasks-container");
+const taskInContainer = document.getElementById("task");
+const inputtedTask = document.getElementById("inputted-task");
 
 function appendTask(text) {
   const task = document.createElement("div");
   task.classList.add("task");
+  if (!text.trim()) return;
 
   const checkbox = document.createElement("input");
   checkbox.type = "checkbox";
+
   const textField = document.createElement("p");
   textField.textContent = text;
+
   const deleteButton = document.createElement("button");
   deleteButton.classList.add("delete");
   deleteButton.innerHTML = '<i class="fa-solid fa-trash"></i>';
+
   const editButton = document.createElement("button");
   editButton.classList.add("edit");
   editButton.innerHTML = '<i class="fa-solid fa-pencil"></i>';
@@ -32,3 +38,26 @@ addBtn.addEventListener("click", () => {
 
   inputTask.value = "";
 });
+
+// inputtedTask.addEventListener("click", () => {
+//   const completedTask = document.getElementById("inputted-task");
+//   if (checkBox.checked === false) {
+//     checkBox.checked = true;
+//     completedTask.classList.add("completed");
+//   } else {
+//     checkBox.checked = false;
+//     completedTask.classList.remove("completed");
+//   }
+// });
+
+taskContainer.addEventListener("click", (e) => {
+  if (e.target.type === "checkbox") {
+    const taskDiv = e.target.closest(".task");
+    const taskText = taskDiv.querySelector("p");
+    taskText.classList.toggle("completed");
+  }
+});
+// checkBox.addEventListener("click", () => {
+//   const checkbox = document.createElement("input");
+//   checkbox.type = "checkbox";
+// });
