4aa98c37e8cd5308c0f71b039c7c3a8238e28145
olesiaa22 <olesiashpotak22@gmail.com>
2025-04-04 13:10:15 +0300

    add possibility to edit a task

--- main.js
+++ main.js
@@ -3,6 +3,8 @@ const addBtn = document.getElementById("add");
 const taskContainer = document.getElementById("tasks-container");
 const taskInContainer = document.getElementById("task");
 const inputtedTask = document.getElementById("inputted-task");
+const deleteBtn = document.getElementById("del");
+const editBtn = document.getElementById("edit");
 
 function appendTask(text) {
   const task = document.createElement("div");
@@ -39,6 +41,21 @@ addBtn.addEventListener("click", () => {
   inputTask.value = "";
 });
 
+// deleteBtn.addEventListener('click', (event)=>{
+//   const delTask = event.target
+//   delTask.classList.remove("task")
+// })
+
+taskContainer.addEventListener("click", (event) => {
+  if (
+    event.target.classList.contains("delete") ||
+    event.target.closest(".delete")
+  ) {
+    const taskDiv = event.target.closest(".task");
+    if (taskDiv) taskDiv.remove();
+  }
+});
+
 // inputtedTask.addEventListener("click", () => {
 //   const completedTask = document.getElementById("inputted-task");
 //   if (checkBox.checked === false) {
@@ -51,10 +68,65 @@ addBtn.addEventListener("click", () => {
 // });
 
 taskContainer.addEventListener("click", (e) => {
+  // complete task
   if (e.target.type === "checkbox") {
-    const taskDiv = e.target.closest(".task");
+    const taskDiv = e.target.closest(".inputted-task");
     const taskText = taskDiv.querySelector("p");
     taskText.classList.toggle("completed");
+  }
+});
+
+taskContainer.addEventListener("click", (event) => {
+  //edit task
+  if (
+    event.target.classList.contains("edit") ||
+    event.target.closest(".edit")
+  ) {
+    const taskDiv = event.target.closest(".task");
+
+    if (taskDiv) {
+      // taskDiv.innerHTML = inputTask.value;
+      // inputTask.remove();
+      const taskText = taskDiv.querySelector("p");
+      const currentText = taskText.textContent;
+
+      //створення інпуту для редагування тексту
+      const inputField = document.createElement("input");
+      inputField.type = "text";
+      inputField.value = currentText;
+      taskDiv.replaceChild(inputField, taskText);
+
+      inputField.addEventListener("blur", () => {
+        taskText.textContent = inputField.value;
+        taskDiv.replaceChild(taskText, inputField);
+
+        if (taskDiv.querySelector("input[type:'checkbox'").checked) {
+          taskText.classList.add(completed);
+        }
+      });
+
+      inputField.addEventListener("keydown", (e) => {
+        if (e.key === "Enter") {
+          taskText.textContent = inputField.value;
+          taskDiv.replaceChild(taskText, inputField);
+
+          if (taskDiv.querySelector("input[type='checkbox").checked) {
+            taskText.classList.add("completed");
+          }
+        }
+      });
+    }
+  }
+
+  if (event.target.type === "checkbox") {
+    const taskDiv = event.target.closest(".task");
+    const taskText = taskDiv.querySelector("p");
+
+    if (event.target.checked) {
+      taskText.classList.add("completed");
+    } else {
+      taskText.classList.remove("completed");
+    }
   }
 });
 // checkBox.addEventListener("click", () => {
